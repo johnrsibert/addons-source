8dfda16ad2e0c141d2f63c7eb52e024090a70faa
John Sibert <johnrsibert@gmail.com>
2022-11-07 14:52:10 -0800

    some options appear in menues

--- TimelineContingency/save.sh
+++ TimelineContingency/save.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-#\cp -uv timelinecontingency.* ~/GrampsTesting/addons-source/TimelineContingency/
-\cp -uv * ~/GrampsTesting/addons-source/TimelineContingency/
 
+echo $#
+echo $0
+echo $1
+SRC="./*"
+DEST="/home/player1/GrampsTesting/addons-source/TimelineContingency/"
+echo $SRC
+echo $DEST
+
+#\cp -uv ~/GrampsTesting/addons-source/TimelineContingency/
+\cp -uv $SRC $DEST
+if [ $# -ge 1 ]; then
+   echo "----------"
+   echo "-m "$1
+   echo $1
+   git -C $DEST commit --dry-run -av "-m "$1
+fi
--- TimelineContingency/timelinecontingency.py
+++ TimelineContingency/timelinecontingency.py
@@ -29,23 +29,6 @@ import string
 # Gramps module
 #
 #------------------------------------------------------------------------
-#from gramps.gen.const import GRAMPS_LOCALE as glocale
-#_ = glocale.translation.gettext
-#from gramps.gen.lib import EventRoleType, EventType, Person, PlaceType, Date
-#from gramps.gen.utils.file import media_path_full
-#from gramps.gen.utils.thumbnails import (get_thumbnail_path, SIZE_NORMAL,
-#                                         SIZE_LARGE)
-#from gramps.gen.plug.report import Report
-#from gramps.gen.plug.report import utils
-#from gramps.gen.plug.report import MenuReportOptions
-#from gramps.gen.plug.report import stdoptions
-#from gramps.gen.plug.menu import (NumberOption, ColorOption, BooleanOption,
-#                                  EnumeratedListOption, PersonListOption,
-#                                  SurnameColorOption)
-#from gramps.gen.utils.db import get_birth_or_fallback, get_death_or_fallback
-#from gramps.gen.proxy import CacheProxyDb
-#from gramps.gen.errors import ReportError
-#from gramps.gen.display.place import displayer as _pd
 
 from gramps.gen.display.name import displayer
 from gramps.gen.display.place import displayer as place_displayer
@@ -59,6 +42,7 @@ from gramps.gen.plug.report import MenuReportOptions
 import gramps.gen.datehandler
 from gramps.gen.relationship import get_relationship_calculator
 from gramps.gen.const import GRAMPS_LOCALE as glocale
+
 try:
     _trans = glocale.get_addon_translator(__file__)
 except ValueError:
@@ -66,31 +50,68 @@ except ValueError:
 _ = _trans.gettext
 
 
-#------------------------------------------------------------------------
-#
-# TimelineContingencyReport -- created once the user presses 'OK'
-#
-#------------------------------------------------------------------------
-class TimelineContingency(Report):
-    """ TimelineContingency report """
 
-    def __init__(self, database, options, user):
-        """
-        Initialize the report.
+'''
+   TimelineContingencyReport -- created once the user presses 'OK'
+   The optionclass defined in the gpr file must inherit from MenuReportOptions.
+'''
+class TimelineContingency(MenuReportOptions):
+    # TimelineContingency report
 
-        @param database: the Gramps database instance
-        @param options: instance of the Options class for this report
-        @param user: a gramps.gen.user.User() instance
-        """
+    RECURSE_NONE = 0
+    RECURSE_SIDE = 1
+    RECURSE_ALL = 2
 
-        Report.__init__(self, database, options, user)
+ 
+    def __init__(self, name, dbase):
+        self.__db = dbase
+        self.__pid = None
+        MenuReportOptions.__init__(self, name, dbase)
+        print('finished __init__')
 
-        menu = options.menu
-        self.person_id    = menu.get_option_by_name('pid').get_value()
-        self.recurse      = menu.get_option_by_name('recurse').get_value()
-        self.callname     = menu.get_option_by_name('callname').get_value()
-        self.placeholder  = menu.get_option_by_name('placeholder').get_value()
-        self.incl_sources = menu.get_option_by_name('incl_sources').get_value()
-        self.incl_notes   = menu.get_option_by_name('incl_notes').get_value()
+#   def get_subject(self):
+    #   self.__filter = FilterOption(_("People"), 0)
+    #   self.__filter.set_help(
+    #       _("Select people to use in the contingency report."))
 
-    print('here')
+        """ Return a string that describes the subject of the report. """
+    #   gid = self.__pid.get_value()
+    #   person = self.__db.get_person_from_gramps_id(gid)
+    #   print('gid,person',gid,person)
+    #   print('returning from get_subject')
+    #   return displayer.display(person)
+
+    def add_menu_options(self, menu):
+
+        ##########################
+        category_name = _("Report Options")
+        ##########################
+
+    #   print(self.__pid)
+        self.__pid = PersonOption(_("People"))
+        print(type(self.__pid))
+        self.__pid.set_help(
+            _("Select people to use in the contingency report."))
+        menu.add_option(category_name, "pid", self.__pid)
+        print(type(self.__pid))
+
+    #   recurse = EnumeratedListOption(_("Print sheets for"), self.RECURSE_NONE)
+    #   recurse.set_items([
+    #       (self.RECURSE_NONE, _("Center person only")),
+    #       (self.RECURSE_SIDE, _("Center person and descendants in side branches")),
+    #       (self.RECURSE_ALL,  _("Center person and all descendants"))])
+    #   recurse.set_help(_("Whether to include descendants, and which ones."))
+    #   print('here 2')
+    #   menu.add_option(category_name, "recurse", recurse)
+
+        '''
+        # --------------------------------
+        add_option = partial(menu.add_option, _('People of Interest'))
+        # --------------------------------
+
+        person_list = PersonListOption(_('People of interest'))
+        person_list.set_help(_('People of interest are used as a starting '
+                               'point when determining "family lines".'))
+        add_option('gidlist', person_list)
+
+        '''
